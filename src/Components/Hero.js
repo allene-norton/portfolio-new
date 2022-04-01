70c2b5740f00fcc12828dcbbda4f6b2cccf25761
Allene Norton <norton.allene@gmail.com>
2022-04-01 13:22:53 -0500

    change hero install router

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div style={{color: '#fff', fontSize: '3em'}}>
       <ReactTypingEffect
-        text={["hi.", "my name is allene.", "you may have already started scrolling.", "but if not...", "i'm glad you're here with me.", "i am a web developer", "currently based in austin, texas.", "i teach at a coding bootcamp.", "i have lots of pets.", "i am also a musician.", "i don't have a favorite food...", "or a favorite anything, really.", "actually...", "i do have a favorite song.", "it is naive melody by the talking heads.", "the live version from stop making sense.",]}
+        text={["Allene Norton", "Full-Stack Web Developer", "Software Development Instructor"]}
         eraseDelay={2000}
         typingDelay={1200}
         speed={80}
